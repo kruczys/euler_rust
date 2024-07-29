512714ffe2f9b9fafbad1e597fffec1535b613de
Konrad Kreczko <konradkreczko@gmail.com>
2024-07-30 00:48:15 +0200

    Done problem 1

--- src/problem_1/solve_1.rs
+++ src/problem_1/solve_1.rs
@@ -1,5 +1,7 @@
 pub fn solve_1(max: i32) -> i32 {
-    let three_multiplies: Vec<i32> = (1..max).map(|x| 3 * x).filter(|x| x < &max).collect();
-    let five_multiples: Vec<i32> = (1..max).map(|x| 5 * x).filter(|x| x < &max).collect();
-    three_multiplies.iter().sum::<i32>() + five_multiples.iter().sum::<i32>()
+    let mut result: Vec<i32> = (1..max).map(|x| 3 * x).filter(|x| x < &max).collect();
+    result.append(&mut (1..max).map(|x| 5 * x).filter(|x| x < &max).collect());
+    result.sort();
+    result.dedup();
+    result.iter().sum::<i32>()
 }
